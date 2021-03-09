ad4e48f2f5b29a65a9617c64154e4bf8ab275c3a
“Abbie <abbie.coghlan@gmail.com>
2021-03-09 12:46:02 -0600

    done

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,19 @@
 // TODO: Create action creators as defined in tests
+const ADD_QUOTE = 'ADD_QUOTE'
+const REMOVE_QUOTE = "REMOVE_QUOTE"
+const UPVOTE_QUOTE = "UPVOTE_QUOTE"
+const DOWNVOTE_QUOTE = "DOWNVOTE_QUOTE"
+
+export function addQuote(quote) {
+    return { type: ADD_QUOTE, quote: { ...quote, votes: 0 } }
+  }
+  export function removeQuote(id) {
+    return { type: REMOVE_QUOTE, quoteId: id};
+  }
+  export function upvoteQuote(id) {
+    return { type: UPVOTE_QUOTE, quoteId: id};
+  }
+  export function downvoteQuote(id) {
+    return { type: DOWNVOTE_QUOTE, quoteId: id};
+  }
+  
